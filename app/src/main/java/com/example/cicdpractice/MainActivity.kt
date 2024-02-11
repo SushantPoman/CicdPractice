60a8800bf5052c2594720ed78c134deeec63dff1
Sushant Poman <sushantpoman94@gmail.com>
2024-02-11 19:31:29 +0530

    Cicd test text added

--- app/src/main/java/com/example/cicdpractice/MainActivity.kt
+++ app/src/main/java/com/example/cicdpractice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cicdpractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -31,10 +32,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text(
+            text = "Testing cicd",
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
